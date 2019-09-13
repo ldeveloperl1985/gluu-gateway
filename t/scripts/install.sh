0ed5968610936052c30b5342774d93785307c2a5
Meghna Joshi <ldeveloperl1985@gmail.com>
2019-09-13 17:59:57 +0530

    Updated the jenkies installation

--- t/scripts/install.sh
+++ t/scripts/install.sh
@@ -15,6 +15,7 @@ function prepareSourcesXenial {
     pkill .*upgrade.*
     rm /var/lib/dpkg/lock
     sleep 60
+    apt-get update
     apt-get install -y apt-transport-https curl lsb-core
     echo "deb https://kong.bintray.com/kong-deb `lsb_release -sc` main" | sudo tee -a /etc/apt/sources.list
     curl -o bintray.key https://bintray.com/user/downloadSubjectPublicKey?username=bintray
